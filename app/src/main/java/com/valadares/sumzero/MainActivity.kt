027da6d2ad0b1bc3a35ec054a16b2629314690f0
Felps <felipe.valadares2@gmail.com>
2020-06-18 20:56:18 -0300

    - Initial sumZero implementation

--- app/src/main/java/com/valadares/sumzero/MainActivity.kt
+++ app/src/main/java/com/valadares/sumzero/MainActivity.kt
@@ -7,6 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sumZero(listOf(-1,0,1,2,3,4))
+    }
+
+    fun sumZero(numbersList: List<Int>): Pair<Int, Int>? {
+        for (number in numbersList) {
+            numbersList.firstOrNull { it + number == 0 }?.let { firstMatch ->
+                return Pair(number, firstMatch)
+            }
+        }
+        return null
     }
 
 }
